d6760b54c4a8cbd81e8fbfc6daf300af76d68b97
Edmund Huber <me@ehuber.info>
2017-04-24 14:40:44 -0700

    use data section rather than the stack

--- test.c
+++ test.c
@@ -2,11 +2,12 @@
 
 #include "jenny.h"
 
+int this_is_an_int = 20;
+char *this_is_a_string = "\"abc\n\t\\123";
+jenny_t test[] =
+#include "test.jenny.c"
+
 int main(void) {
-    int this_is_an_int = 20;
-    char *this_is_a_string = "\"abc\n\t\\123";
-    jenny_t test[] =
-    #include "test.jenny.c"
     jenny(test, write, STDOUT_FILENO);
 
     return 0;
